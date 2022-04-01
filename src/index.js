4eaf2bb4a4e44328fe6d509b80b0f1c564e812fe
App Academy Student <email@email.com>
2022-04-01 17:59:24 -0400

    add sidebar section and float body to the right

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+import dataUtils from './scripts/dataUtil.js';
 
 
 // the event listener's callback will be called AFTER the DOM is fully loaded.
 // The anonymous function **is not** called right at this point.
 document.addEventListener("DOMContentLoaded", () => {
-    const dataUtil = require("./scripts/dataUtil.js"); 
-    
-    // let wrapper  = document.getElementById("main"); 
-    // wrapper.innerHTML = "<p>Tester</p>"; 
+  // const dataUtils = require("./scripts/dataUtil.js"); 
+    let my_data = new dataUtils(); 
   
 
+    let coinsList;  
+    // my_data.coinsList().then((data) => {
+    //   coinsList = data; 
+    //   console.log(coinsList);
+    // });
+    
+    let simplePrice; 
+    
+    my_data.simplePrice().then((data) => {
+      coinsList = data; 
+      console.log(coinsList);
+    });
+    
+    
+
+    // let wrapper  = document.getElementById("main"); 
+    // wrapper.innerHTML = "<p>Tester</p>"; 
+    
+
   }); 
+
+
+
+
--- src/scripts/dataUtil.js
+++ src/scripts/dataUtil.js
@@ -1,38 +1,47 @@
 console.log("hello I'm dataUtil"); 
 
 let root_url = "https://api.coingecko.com/api/v3"; 
-
 let simple_price_endpoint = root_url + "/simple/price?ids=bitcoin&vs_currencies=usd"; 
-let coins_list_endpoint = root_url + "/coins/list"; 
 let coin_endpoint = root_url + "/coins/bitcoin"; 
-
 let my_data; 
 
-// fetch(simple_price_endpoint)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     my_data = data; 
-//     ele.innerHTML = `Today's bitcoin price is: ${my_data.bitcoin.usd}`; 
-//   });
+class DataUtil { 
 
-// fetch(coins_list_endpoint)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data);
-//     my_data = data; 
-//     ele.innerHTML = `The coins list has this many coins lol: ${my_data.length}`; 
-//   });
+  constructor() { 
+    // this.ele = document.querySelector("#test-display");
+  }
+  coinsList(){
 
-  fetch(coin_endpoint)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    my_data = data; 
-    ele.innerHTML = `The bitcoin symbol is: <img src='${my_data.image.thumb}' />`;
-  });
+    let coins_list_endpoint = root_url + "/coins/list"; 
+    return fetch(coins_list_endpoint)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        this.ele.innerHTML = `The coins list has this many coins lol: ${data.length}`; 
+        return data; 
+      });
 
+  }
 
-let ele = document.querySelector("#test-display"); 
+  simplePrice(){
+    return fetch(simple_price_endpoint)
+      .then(response => response.json())
+      .then(data => {
+        this.ele.innerHTML = `Today's bitcoin price is: ${data.bitcoin.usd}`; 
+        return data; 
+      });
+  }
 
+  coinInfo(){
+    return fetch(coin_endpoint)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      ele.innerHTML = `The bitcoin symbol is: <img src='${data.image.thumb}' />`;
+    });
+  }
+
+}
+
+export default DataUtil;  
 
