777933854040cea9b56ebd62cac1490a4130958e
ds <swaby@protonmail.com>
2022-04-02 14:10:43 -0400

    finish remove coin from portfolio functionality

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -28,14 +28,31 @@ class Portfolio{
         }
     }
 
+    removeCoin(coin){
+        let port = this.portfolio.filter((ele) => {
+            return ele.id !== coin.id
+        }); 
+        this.portfolio = port;
+        localStorage.setItem("portfolio", JSON.stringify(port)); 
+        this.updatePortList();
+
+    }
+
     updatePortList(){
         if(this.portfolio.length === 0) return;
         let list = document.querySelector("#port-list"); 
         list.innerHTML = ''; 
         let child; 
+        let button; 
         this.portfolio.forEach((ele) => {
             child = document.createElement('li')
+            button = document.createElement('button');
+            button.innerHTML = 'remove'
+            button.addEventListener("click", (e) => {
+                this.removeCoin(ele);
+            })
             child.innerHTML = ele.id; 
+            child.appendChild(button);
             list.appendChild(child);
         })
     }
